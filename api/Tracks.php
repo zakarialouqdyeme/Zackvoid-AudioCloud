b396f41626b98cd7baeb92506b6ee1ebcb148442
ZACK VOID <zakarialouqdyeme@gmail.com>
2021-03-16 03:36:59 +0100

    tracks

--- api/Tracks.php
+++ api/Tracks.php
@@ -16,7 +16,6 @@ function getTrackById($conn)
         $base64Image = base64_encode($row["image"]);
         $tampTrack = new apiTrack($row["idt"], $row["title"], $base64Image, $row["description"], $generateUrl);
         array_push($arrayData, $tampTrack);
-
     }
     header('Content-Type: application/json');
     return json_encode($arrayData, JSON_PRETTY_PRINT);
@@ -24,10 +23,25 @@ function getTrackById($conn)
 
 
 
-function getTrackByName($name)
+function getTrackByTitle($conn)
 {
-    $name = $_GET["name"];
+    $title = $_GET["title"];
+    $sql = "select * from track where title = $title";
+    $arrayData = array();
+    $result = $conn->query($sql);
+    while ($row = $result->fetch_assoc()) {
+
+        $directory = explode("/", dirname(__DIR__));
+        $dirIndex = sizeof($directory);
+        $generateUrl = $_SERVER['HTTP_HOST'] . "/" . $directory[$dirIndex - 1] . "/uploads/" . $row["filename"];
+        $base64Image = base64_encode($row["image"]);
+        $tampTrack = new apiTrack($row["idt"], $row["title"], $base64Image, $row["description"], $generateUrl);
+        array_push($arrayData, $tampTrack);
+    }
+    header('Content-Type: application/json');
+    return json_encode($arrayData, JSON_PRETTY_PRINT);
 }
+
 
 function getTracks($conn)
 {
@@ -42,14 +56,16 @@ function getTracks($conn)
         $base64Image = base64_encode($row["image"]);
         $tampTrack = new apiTrack($row["idt"], $row["title"], $base64Image, $row["description"], $generateUrl);
         array_push($arrayData, $tampTrack);
-        
     }
     header('Content-Type: application/json');
     return json_encode($arrayData, JSON_PRETTY_PRINT);
 }
 
-if (isset($_GET["id"])){
+if (isset($_GET["id"])) {
     echo getTrackById($conn);
-}else{
+}
+if (isset($_GET["name"])) {
+    echo getTrackByTitle($conn);
+} else {
     echo getTracks($conn);
 }
