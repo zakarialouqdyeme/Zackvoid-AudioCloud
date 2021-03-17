1356d3f159753e96624a522600f089c133c6ba96
ZACK VOID <zakarialouqdyeme@gmail.com>
2021-03-17 12:54:07 +0100

    PlaylistApi

--- api/Playlists.php
+++ api/Playlists.php
@@ -5,18 +5,16 @@ include '../models/tracksModel.php';
 
 function getPlaylistById($conn)
 {
-
-    $userId = $_GET["id"];
-
+    $id = $_GET["id"];
     $playlists = array();
     $tempPlaylist = array();
-    $sql = "select * from playlist where userId = '$userId' ";
+    $sql = "select * from playlist where idp = '$id' ";
     $result = $conn->query($sql);
     $result_num = $result->num_rows;
 
     if ($result_num < 1) {
 
-        echo 'Empty';
+        echo '404';
     } else {
 
         while ($row = $result->fetch_assoc()) {
@@ -53,11 +51,90 @@ function getPlaylistById($conn)
 
 function getPlaylistByName($conn)
 {
+    $name = $_GET["name"];
+    $playlists = array();
+    $tempPlaylist = array();
+    $sql = "select * from playlist where name = '$name' ";
+    $result = $conn->query($sql);
+    $result_num = $result->num_rows;
+
+    if ($result_num < 1) {
+
+        echo '404';
+    } else {
+
+        while ($row = $result->fetch_assoc()) {
+
+            $tracksTempArray = array();
+
+            $tempPlaylist = new playlist(array("idp" => $row["idp"], "name" => $row["name"], "userId" => $row["userId"]));
+
+            $idp = $row["idp"];
+            $sql2 = "select * from track t, tp ttp where t.idt = ttp.idt and idp = '$idp' ";
+            $result2 = $conn->query($sql2);
+            $result2_num = $result2->num_rows;
+
+            if ($result2_num > 0) {
+
+                while ($row = $result2->fetch_assoc()) {
+                    $directory = explode("/", dirname(__DIR__));
+                    $dirIndex = sizeof($directory);
+                    $generateUrl = $_SERVER['HTTP_HOST'] . "/" . $directory[$dirIndex - 1] . "/uploads/" . $row["filename"];
+                    $base64Image = base64_encode($row["image"]);
+                    array_push($tracksTempArray, new apiTrack($row["idt"], $row["title"], $base64Image, $row["description"], $generateUrl));
+                }
+            }
+
+            array_push($tempPlaylist->tracks, $tracksTempArray);
+            array_push($playlists, $tempPlaylist);
+        }
+        header('Content-Type: application/json');
+        return json_encode($playlists, JSON_PRETTY_PRINT);
+    }
 }
 
 
 function getPlaylists($conn)
 {
+    $playlists = array();
+    $tempPlaylist = array();
+    $sql = "select * from playlist";
+    $result = $conn->query($sql);
+    $result_num = $result->num_rows;
+
+    if ($result_num < 1) {
+
+        echo '404';
+    } else {
+
+        while ($row = $result->fetch_assoc()) {
+
+            $tracksTempArray = array();
+
+            $tempPlaylist = new playlist(array("idp" => $row["idp"], "name" => $row["name"], "userId" => $row["userId"]));
+
+            $idp = $row["idp"];
+            $sql2 = "select * from track t, tp ttp where t.idt = ttp.idt and idp = '$idp' ";
+            $result2 = $conn->query($sql2);
+            $result2_num = $result2->num_rows;
+
+            if ($result2_num > 0) {
+
+                while ($row = $result2->fetch_assoc()) {
+                    $directory = explode("/", dirname(__DIR__));
+                    $dirIndex = sizeof($directory);
+                    $generateUrl = $_SERVER['HTTP_HOST'] . "/" . $directory[$dirIndex - 1] . "/uploads/" . $row["filename"];
+                    $base64Image = base64_encode($row["image"]);
+                    array_push($tracksTempArray, new apiTrack($row["idt"], $row["title"], $base64Image, $row["description"], $generateUrl));
+                }
+            }
+
+            array_push($tempPlaylist->tracks, $tracksTempArray);
+            array_push($playlists, $tempPlaylist);
+        }
+        header('Content-Type: application/json');
+        return json_encode($playlists, JSON_PRETTY_PRINT);
+    }
 }
 
 if (isset($_GET["id"])) {
